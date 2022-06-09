abfcb42c2c3b80590d7042c48f17fa7dc66c81c5
kanaru-ssk <2019january.0131@gmail.com>
2022-06-10 00:19:40 +0900

    delete unused const

--- src/app.h
+++ src/app.h
@@ -4,15 +4,6 @@
 #define Y 1
 #define Z 2
 
-#define DIV_W 32
-#define DIV_H 32
-#define NUM_POINTS DIV_W *DIV_H
-#define NUM_INDEX 2 * (DIV_W + 1) * (DIV_H - 1)
-
-#define PI 3.141592653589793
-#define M 1.0
-#define G 9.80665
-
 class app
 {
 public:
